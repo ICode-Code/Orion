f183ee8a1559eac05789e658d91ceefff9f2462c
Job <eyobamane09@gmail.com>
2023-09-28 18:00:42 -0700

    Awesome now the log  save external txt file about the log, it is pritty much selfe explantory coool

--- Log/src/Log.cpp
+++ Log/src/Log.cpp
@@ -3,11 +3,15 @@
 namespace OE1Core
 {
 	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_FileLogger;
 	void Log::Init()
 	{
+
 		spdlog::set_pattern("%^[%T] %n: %v%$"); 
 
-		s_CoreLogger = spdlog::stdout_color_mt("ORION_CORE");
+		s_CoreLogger = spdlog::stdout_color_mt("[ORI_CORE] ");
+		s_FileLogger = spdlog::basic_logger_mt("[ECHO] ", "ORI_log.txt", true);
 		s_CoreLogger->set_level(spdlog::level::trace);
+		s_FileLogger->set_level(spdlog::level::trace);
 	}
 }
--- Log/src/Log.h
+++ Log/src/Log.h
@@ -5,6 +5,7 @@
 #include <memory>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 
 namespace OE1Core
@@ -13,17 +14,44 @@ namespace OE1Core
 	{
 	public:
 		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetFileLogger() { return s_FileLogger; }
 		static void Init();
 
 	private:
 		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_FileLogger;
 	}; 
 }
 
-#define LOG_INFO(...)         ::OE1Core::Log::GetCoreLogger()->info(__VA_ARGS__);
-#define LOG_FATAL(...)        ::OE1Core::Log::GetCoreLogger()->fatal(__VA_ARGS__);
-#define LOG_TRACE(...)        ::OE1Core::Log::GetCoreLogger()->trace(__VA_ARGS__);
-#define LOG_WARRNING(...)     ::OE1Core::Log::GetCoreLogger()->warn(__VA_ARGS__);
-#define LOG_ERROR(...)        ::OE1Core::Log::GetCoreLogger()->error(__VA_ARGS__);
+#define LOG_INFO(...)   \
+do {\
+		::OE1Core::Log::GetCoreLogger()->info(__VA_ARGS__); \
+		::OE1Core::Log::GetFileLogger()->info(__VA_ARGS__); \
+   } while(0) 
+
+#define LOG_FATAL(...) \
+do { \
+		::OE1Core::Log::GetCoreLogger()->fatal(__VA_ARGS__); \
+		::OE1Core::Log::GetFileLogger()->fatal(__VA_ARGS__); \
+} while (0)
+
+#define LOG_TRACE(...) \
+do { \
+		::OE1Core::Log::GetCoreLogger()->trace(__VA_ARGS__); \
+		::OE1Core::Log::GetFileLogger()->trace(__VA_ARGS__); \
+} while (0)
+
+#define LOG_WARRNING(...) \
+do { \
+		::OE1Core::Log::GetCoreLogger()->warn(__VA_ARGS__); \
+		::OE1Core::Log::GetFileLogger()->warn(__VA_ARGS__); \
+} while (0)
+
+#define LOG_ERROR(...)  \
+do { \
+		::OE1Core::Log::GetCoreLogger()->error(__VA_ARGS__); \
+		::OE1Core::Log::GetFileLogger()->error(__VA_ARGS__); \
+} while (0)
+
 
 #endif // !OE1_LOG_H_
